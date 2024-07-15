4abcd9dc963f15906355f5a0580f086addd86f7e
Esmael Abdlkadr <esmaelabdlkadr00@gmail.com>
2024-07-15 16:06:51 +0300

    file upload to cludinary implemented

--- server/app.ts
+++ server/app.ts
@@ -6,16 +6,13 @@ import hotelRoute from "./src/routes/hotelRoute";
 import adminRoute from "./src/routes/adminRoute";
 import { errorMiddleware } from "./src/utils/error";
 import path from "node:path";
-import { v2 as cloudinary } from "cloudinary";
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
+import { cloudinaryConfig } from "./src/config/cloudinaryConfig";
+
 const app: Express = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cloudinaryConfig);
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 // production script.
 app.use(express.static(path.join(__dirname, "../../client/dist")));
