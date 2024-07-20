38e29c31078c1d01b7aaa1feb9c87b73f3c39ea1
Esmael Abdlkadr <esmaelabdlkadr00@gmail.com>
2024-07-20 18:37:22 +0300

    sidebar updated

--- client/src/admin/dashbaord/DashboardHome.tsx
+++ client/src/admin/dashbaord/DashboardHome.tsx
@@ -1,4 +1,4 @@
-import {useSidebarContext} from "@/contexts/SidebarContext";
+import { useSidebarContext } from "@/contexts/SidebarContext";
 import {
     HiMiniUser,
     HiOutlineArrowRightOnRectangle,
@@ -6,38 +6,84 @@ import {
     HiOutlineBolt,
     HiOutlineCog8Tooth
 } from "react-icons/hi2";
-import {useState} from "react";
-import {HiOutlineChevronDown, HiOutlineUser} from "react-icons/hi";
+import { useEffect, useRef, useState } from "react";
+import { HiOutlineChevronDown, HiOutlineUser } from "react-icons/hi";
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 
-const DashboardHome=()=>{
-    const{isCollapsed}=useSidebarContext()
+} from "@/components/ui/dropdown-menu"
+const DashboardHome = () => {
+    const { isCollapsed } = useSidebarContext();
     const [isOpen, setIsOpen] = useState(false);
+    const[position,setPosition]=useState("bottom")
+    const dropDownRef = useRef<HTMLDivElement>(null);
 
-    const toggleDropdown = () => setIsOpen(!isOpen)
-    return(
+    const toggleDropdown = () => setIsOpen(!isOpen);
 
-        <div style={{marginLeft:isCollapsed?"80px":"300px"}} className={"bg-[#e2e8f0]"}>
-            <div className={"w-full  h-[80px]  bg-red-500"}>
-                <div className="relative font-sans w-max mx-auto">
+    // Global click event listener to close the dropdown
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (dropDownRef.current && !dropDownRef.current.contains(event.target as Node)) {
+                setIsOpen(false);
+            }
+        };
+        const handleEscapeKey = (event: KeyboardEvent) => {
+            if (event.key === "Escape") {
+                setIsOpen(false);
+            }
+        };
+        document.addEventListener("keydown", handleEscapeKey);
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+            document.removeEventListener("keydown", handleEscapeKey);
+        };
+    }, []);
+
+    return (
+        <div style={{ marginLeft: isCollapsed ? "80px" : "300px" }} className={"bg-[#e2e8f0]  flex gap-5 flex-between"}>
+            <div>
+                <h6>Dashboard</h6>
+            </div>
+            {/*flex-2*/}
+            <div className={"flex items-center  gap-4  flex-end"}>
+                <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline">Open</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+                        <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+                    </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
+                <div className="relative font-sans w-max mx-auto" ref={dropDownRef}>
                     <button
                         type="button"
                         onClick={toggleDropdown}
-                        className="px-6 py-3 rounded text-white text-sm font-semibold border-none outline-none  flex  ietms-center   gap-4  bg-white"
+                        className="px-6 py-3 rounded text-white text-sm font-semibold border-none outline-none flex items-center gap-4 bg-white"
                     >
-
-
-                        <HiMiniUser size={30} className={"text-white bg-[#2563eb]  rounded-full"}/>
-                        <div className={"flex flex-col "}>
+                        <HiMiniUser size={30} className={"text-white bg-[#2563eb] rounded-full"}/>
+                        <div className={"flex flex-col"}>
                             <span className={"text-xs text-[#3b82f6]"}>Administrator</span>
-                            <span className={"text-slate-700  font-semibold"}>Hakuna Matata</span>
+                            <span className={"text-slate-700 font-semibold"}>Hakuna Matata</span>
                         </div>
-
-                        <HiOutlineChevronDown size={25} color={"white"} className={"mb-3"} />
+                        <HiOutlineChevronDown size={25} color={"white"} className={"mb-3"}/>
                     </button>
 
-                    <ul className={`absolute ${isOpen ? 'block' : 'hidden'} 
-                    shadow-lg bg-white py-2 z-[1000] min-w-full w-max rounded max-h-96 overflow-auto`}>
-                        <li className='flex items-center py-3 px-6  bg-[#e2e8f0] hover:bg-blue-50 text-black text-sm cursor-pointer'>
+                    <ul className={`absolute ${isOpen ? 'block' : 'hidden'} shadow-lg bg-white py-2 z-[1000] min-w-full w-max rounded max-h-96 overflow-auto`}>
+                        <li className='flex items-center py-3 px-6 bg-[#e2e8f0] hover:bg-blue-50 text-black text-sm cursor-pointer'>
                             <div className="flex flex-wrap items-center justify-center gap-4 cursor-pointer">
                                 <img src="https://readymadeui.com/team-1.webp" className="w-12 h-12 rounded-full"
                                      alt="John Doe"/>
@@ -46,34 +92,35 @@ const DashboardHome=()=>{
                                     <p className="text-xs text-gray-500 mt-0.5">johndoe23@gmail.com</p>
                                 </div>
                             </div>
-
                         </li>
                         <div className="h-[1px] bg-gray-300 w-full"></div>
-                        <li className='flex items-center  gap-3 py-3 px-6 hover:bg-blue-50   cursor-pointer text-[#334155]'>
+                        <li className='flex items-center gap-3 py-3 px-6 hover:bg-blue-50 cursor-pointer text-[#334155]'>
                             <HiOutlineUser size={25}/>
-                            view profile
+                            View Profile
                         </li>
-                        <li className='flex items-center gap-3 py-3 px-6 hover:bg-blue-50 text-[#334155]cursor-pointer'>
+                        <li className='flex items-center gap-3 py-3 px-6 hover:bg-blue-50 cursor-pointer text-[#334155]'>
                             <HiOutlineCog8Tooth size={25}/>
                             Account Setting
                         </li>
-                        <li className='flex items-center  gap-3 py-3 px-6   text-[#334155]hover:bg-blue-50 text-black text-sm cursor-pointer'>
+                        <li className='flex items-center gap-3 py-3 px-6 hover:bg-blue-50 cursor-pointer text-[#334155]'>
                             <HiOutlineBolt size={25}/>
                             Login Activity
                         </li>
                         <div className="h-[1px] bg-gray-300 w-full"></div>
-                        <li className='flex items-center  gap-3 py-3 px-6   text-[#334155]hover:bg-blue-50 text-black text-sm cursor-pointer'>
+                        <li className='flex items-center gap-3 py-3 px-6 hover:bg-blue-50 cursor-pointer text-[#334155]'>
                             <HiOutlineArrowRightOnRectangle size={25}/>
-                            Sign out
+                            Sign Out
                         </li>
                     </ul>
                 </div>
+                <div>
+                    <HiOutlineBell/>
+                </div>
             </div>
-            <HiOutlineBell />
+
 
         </div>
+    );
+};
 
-    )
-
-}
 export default DashboardHome;
